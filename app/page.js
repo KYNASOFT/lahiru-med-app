4396d7b1f764cb26a013c488f822ae1b48310411
dinna-i <it22025326@my.sliit.lk.com>
2024-01-24 20:39:18 +0530

    I have added h.w

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
 
 export default function Home() {
   return (
-    <div><h1>Home Page bu Kanchana</h1></div>
+    <div>
+      <h1>Home Page bu Kanchana</h1>
+      <h1> hello world</h1>
+      </div>
+    
 
     // we gonna make this app.
   );
