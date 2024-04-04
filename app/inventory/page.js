ca96a6b85694c0f880ab0bda6db81c589052418f
unknown <IT22909978@my.sliit.lk>
2024-04-04 15:35:22 +0530

    add inventory

---
+++ app/inventory/page.js
@@ -0,0 +1,10 @@
+import Inventory from '@/components/Inventory/Inventory'
+import React from 'react'
+
+function page() {
+  return (
+    <div><Inventory /></div>
+  )
+}
+
+export default page
---
+++ components/Inventory/Inventory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Inventory() {
+  return (
+    <div>hello world</div>
+  )
+}
+
+export default Inventory
