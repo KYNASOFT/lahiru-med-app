67e22a249d74debbcd5032dedf6e5cf5d968d826
unknown <sithmirathnyaka@gmail.com>
2024-04-21 15:16:40 +0530

    I added a p tag

--- app/page.js
+++ app/page.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function page() {
   return (
-    <div>This is home page</div>
+    <div>This is home page
+      <p>Sithmi Branch</p>
+    </div>
+
     //I changed
   )
 }
